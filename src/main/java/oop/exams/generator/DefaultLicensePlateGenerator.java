ce50bc03c606a338761a77952efa1624929569ea
EythanDuran <eythan96@hotmail.com>
2021-11-11 08:56:04 -0600

    intento de resolucion

---
+++ src/main/java/oop/exams/generator/DefaultLicensePlateGenerator.java
@@ -0,0 +1,9 @@
+package oop.exams.generator;
+
+public class DefaultLicensePlateGenerator implements LicensePlateGenerator {
+
+    @Override
+    public String generate(String state) {
+        return null;
+    }
+}
--- src/main/java/oop/exams/generator/LicensePlateGenerator.java
+++ src/main/java/oop/exams/generator/LicensePlateGenerator.java
@@ -3,4 +3,5 @@ package oop.exams.generator;
 public interface LicensePlateGenerator {
 
     String generate(String state);
+
 }
